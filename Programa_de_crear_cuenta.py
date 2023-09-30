29753ca1a47d2910541365268834debc8bf090f3
PEP1 <jordyventura91@gmail.com>
2023-09-30 15:59:06 -0600

    Primera etapa de la implementacion de la validacion de datos

--- Programa_de_crear_cuenta.py
+++ Programa_de_crear_cuenta.py
@@ -1,7 +1,11 @@
 from tkinter import*
+import re
+import datetime
+
 
 
 def nueva_cuenta(numero_de_casa_ganadora, frame_nueva_cuenta, imagen, nombre, apellido, correo, username, dpi, telefono, fecha, password, confirmacion):
+    #Posicion de los widgets en la pantalla
     cortx = 400
     cory = 100
     
@@ -125,10 +129,141 @@ def nueva_cuenta(numero_de_casa_ganadora, frame_nueva_cuenta, imagen, nombre, ap
     lista_recibida = [nombre, apellido, correo, username, dpi, telefono, fecha, password, confirmacion]
     return lista_recibida
     
-    
+
+#Funcion que imprime los datos que se escribieron en el campo de texto   
 def recibir_datos_de_crear_cuenta (lista_de_recepcion):
     lista_recibida = []
     for elemento in lista_de_recepcion:
         valor = elemento.get()
         lista_recibida.append(valor)  # No necesitas .get() aquí
     print(lista_recibida)
+    
+    #Verificacion de los datos
+    ver_nombre = lista_recibida[0]
+    ver_apellido = lista_recibida[1]
+    ver_correo = lista_recibida[2]
+    ver_DPI = lista_recibida[4]
+    ver_telefono = lista_recibida[5]
+    ver_fecha = lista_recibida[6]
+    ver_password = lista_recibida[7]
+    ver_confirmacion = lista_recibida[8]
+    
+    
+    print(ver_nombre)
+    print(ver_apellido)
+    print(ver_correo)
+    print(ver_DPI)
+    print(ver_telefono)
+    print(ver_fecha)
+    print(ver_password)
+    print(ver_confirmacion)
+    
+    #Nombre
+    if ver_nombre.isalpha():
+        print("El nombre contiene solo letras.")
+    else:
+        print("El nombre contiene otro tipo de datos.")
+    
+    #Apellido
+    if ver_apellido.isalpha():
+        print("El apellido contiene solo letras")
+    else:
+        print("El apellido contiene otro tipo de letras")
+        
+    #DPI
+    cantidad_diguitos_dpi = len(ver_DPI)
+    if cantidad_diguitos_dpi == 13:
+        if ver_DPI.isdigit():
+            print("Si puede ser un numero de DPI")
+        else:
+            print("Faltan diguitos de un numero de DPI")
+    else:
+        print("No puede ser un numero de DPI")
+    
+        
+
+    # Telefono
+    cantidad_digitos_tel = len(ver_telefono)
+    if cantidad_digitos_tel == 8:
+        if ver_telefono.isdigit():
+            print("Si puede ser un numero telefonico")
+        else:
+            print("No es un numero telefonico valido")
+    else:
+        print("No tiene 8 digitos")
+        print(cantidad_digitos_tel)
+        
+    #Direccion de correo
+    # Patrón de expresión regular para validar un correo de Gmail
+    patron = r'^[a-zA-Z0-9._%+-]+@gmail\.com$'
+
+    #Usar re.match() para comprobar si el correo coincide con el patrón
+    if re.match(patron, ver_correo):
+        print("Correo es valido")
+    else:
+        print("Correo invalido")
+        
+    #Fecha de nacimiento
+        # Dividir la cadena en partes usando '/' como separador
+    partes = ver_fecha.split('/')  
+    #Verificar si hay exactamente 3 partes y si las partes 0, 1 y 2 son dígitos
+    if len(partes) == 3 and partes[0].isdigit() and partes[1].isdigit() and partes[2].isdigit():
+        #Verificar que las partes 0, 1 y 2 tengan longitudes válidas (2, 2, y 4 caracteres respectivamente)
+        if len(partes[0]) in [1, 2] and len(partes[1]) in [1, 2] and len(partes[2]) == 4:
+            print("formato de  fecha valida")
+        else:
+            print("formato de  fecha invalida")
+    else: 
+        print("formato de fecha Invalida")
+        
+    now = datetime.datetime.now()
+    year_actual = now.strftime("%Y")
+    year_nacimiento = partes[2]
+    year_actual_int = int(year_actual)
+    year_nacimiento_int = int(year_nacimiento)
+    edad = year_actual_int - year_nacimiento_int
+    print("Year actual: " + str(year_actual_int))
+    print("year nacimiento: " + str(year_nacimiento_int))
+    print("edad: " + str(edad))
+    
+
+    if 11 <= edad <= 18:
+        print("Tiene la edad suficiente")
+    else:
+        print("No tiene la edad suficiente")
+    
+    
+    # #Contraseña
+    carnumero = 0
+    carletramayuscula = 0
+    carletraminuscula = 0
+    car_letras_especiales = 0
+    caracteres_especiales = ['!', '#', '$', '%', '&', '/', '(', ')', '=', '?', '¿', '[', '*', '{', '}', '/', '+', '-', '.', ',', '.', '-']
+    cantidad_diguitos = len(ver_password)
+    
+    if cantidad_diguitos >= 8:
+        for posicion in ver_password:                
+            if 48 <= ord(posicion) <= 57:
+                carnumero += 1
+            if 'A' <= posicion <= 'Z':
+                carletramayuscula += 1
+            if 'a' <= posicion <= 'z':
+                carletraminuscula += 1
+            if posicion in caracteres_especiales:
+                car_letras_especiales += 1
+        if carnumero >= 1 and carletramayuscula >= 1 and carletraminuscula >= 1 and car_letras_especiales >= 1:
+            print("Clave valida")
+            
+        else:
+            print("La clave no es valida debe de usar numeros, letras mayusculas, minusculas, y simbolos especiales como: !#%...")
+                
+        #Confirmacion
+        if ver_password == ver_confirmacion:
+            print("La verificacion es la misma que el password")
+        else:
+            print("La confirmacion no es igual que el password")
+    else:
+        print("Cantidad de diguitos insuficientes")
+    
+    
+    
